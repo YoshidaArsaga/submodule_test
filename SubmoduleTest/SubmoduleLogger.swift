73ed7d840e1fcdcc76d2b3a7998472d292b618d2
Hiroki Yoshida <h_yoshida@arsaga.jp>
2021-01-28 16:51:57 +0900

    サンプルメソッドの実装

---
+++ SubmoduleTest/SubmoduleLogger.swift
@@ -0,0 +1,14 @@
+//
+//  SubmoduleLogger.swift
+//  SubmoduleTest
+//
+//  Created by 吉田寛規 on 2021/01/28.
+//
+
+import Foundation
+
+open class SubmoduleLogger {
+    func log() {
+        print("submodule呼ばれたよ！")
+    }
+}
